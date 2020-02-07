36a13b0c521f056bdb1ff6b107913057665b63bf
spikefairway <k.matsubara91@gmail.com>
2020-02-07 17:16:29 +0900

    Define OxfordCat class.

--- data_loader.py
+++ data_loader.py
@@ -5,6 +5,8 @@ from PIL import Image
 import torch
 import os
 import random
+import pandas as pd
+import numpy as np
 
 
 class CelebA(data.Dataset):
@@ -67,6 +69,75 @@ class CelebA(data.Dataset):
         """Return the number of images."""
         return self.num_images
 
+class OxfordCat(data.Dataset):
+    """Dataset class for cat pictures from Oxford IIIT-PET dataset."""
+
+    def __init__(self, image_dir, cond_tab_path, selected_attrs, transform, crop_size=256):
+        """Initialize and preprocesss the OxfordCat dataset.
+
+        Parameters
+        ----------
+        image_dir : string
+            Path for image directory
+        cond_tab_path : string
+            Path for CSV file including condition information
+        selected_attrs : list
+            Attributes to learn or test
+        transform : torchvision.transform
+            Transformations
+        crop_size : int
+            Size to crop
+        """            
+        self.image_dir = image_dir
+        self.cond_tab_path = cond_tab_path
+        self.selected_attrs = selected_attrs
+        self.transform = transform
+        self.crop_size = crop_size
+
+        self.preprocess()
+
+    def preprocess(self):
+        """Preprocessing.
+        """      
+        # Extracted data with selected conditions
+        tab0 = pd.read_csv(self.cond_tab_path)
+        selected_attrs = \
+            tab0['BreedName'].unique() if self.selected_attrs is None \
+            else self.selected_attrs
+        cond_tab = tab0.loc[tab0['BreedName'].isin(selected_attrs), :].copy()
+        cond_tab.reset_index(inplace=True, drop=False)
+
+        # Image path list
+        self.img_path_list = [
+            os.path.join(self.image_dir, '.'.join((img_name, 'jpg')))
+            for img_name in cond_tab['ImageName'].values
+        ]
+
+        # Condition matrix
+        self.cond_mat = cond_tab.loc[:, selected_attrs].values
+
+        print('Finished preprocessing the Oxford Cat dataset')
+
+    def __getitem__(self, idx):
+        img_path = self.img_path_list[idx]
+        c = self.cond_mat[[idx], :]
+        transform = self.transform
+
+        image = Image.open(img_path)
+
+        img_size_min = np.minimum(*image.size)
+        # Add Resize if image_size is smaller than crop_size
+        if img_size_min < self.crop_size:
+            frac = float(self.crop_size) / img_size_min
+            transform = T.Compose([
+                T.Resize(frac),
+                transform
+            ])
+
+        return transform(image), torch.FloatTensor(c)
+
+    def __len__(self):
+        return len(self.img_path_list)
 
 def get_loader(image_dir, attr_path, selected_attrs, crop_size=178, image_size=128, 
                batch_size=16, dataset='CelebA', mode='train', num_workers=1):
@@ -90,3 +161,8 @@ def get_loader(image_dir, attr_path, selected_attrs, crop_size=178, image_size=1
                                   shuffle=(mode=='train'),
                                   num_workers=num_workers)
     return data_loader
+
+def get_loader_oxford(image_dir, cond_tab_path, selected_attrs, crop_size=256, image_size=128,
+                      batch_size=16, mode='train', num_workers=1):
+    """Build and return a data loader for Oxford cat dataset.
+    """
