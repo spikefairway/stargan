4113a17e3db45fa16d96711584d065b66fa7aa00
spikefairway <k.matsubara91@gmail.com>
2020-02-12 14:57:46 +0900

    Download annotations.

--- download_oxford.sh
+++ download_oxford.sh
@@ -5,3 +5,10 @@ mkdir -p $OUTDIR
 wget -N $URL -O $TAR_FILE
 tar xvzf $TAR_FILE -C $OUTDIR
 rm $TAR_FILE
+
+# Annotation data
+URL='https://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz'
+TAR_FILE=./data/oxford.annotations.tar.gz
+wget -N $URL -O $TAR_FILE
+tar xvzf $TAR_FILE -C $OUTDIR
+rm $TAR_FILE
